75cf96770822735b9d0c30a6f296637bbd3740f6
sillyck <ribellas2002@gmail.com>
2022-03-01 12:30:34 +0100

    COmmit Ejercicio1
    Commit Ejercicio1

--- UD26_2/src/main/java/main/dao/IAsignado_aDAO.java
+++
@@ -1,9 +0,0 @@
-package main.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import main.dto.Asignado_a;
-
-public interface IAsignado_aDAO extends JpaRepository<Asignado_a, Integer> {
-
-}
--- UD26_2/src/main/java/main/dao/ICientificosDAO.java
+++
@@ -1,9 +0,0 @@
-package main.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import main.dto.Cientificos;
-
-public interface ICientificosDAO extends JpaRepository<Cientificos, Integer>{
-
-}
